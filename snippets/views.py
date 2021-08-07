af457d8a01063b5e3e702262f551ce7e4881fd07
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2021-08-07 12:30:48 +0200

    Object Level Permissions

--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,23 @@
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 
 # We also need to add two new read-only views for a list of all users 
@@ -20,19 +26,12 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     
-    # Currently there is no way to automatically associate the logged-in user 
-    # that created a snippet with the snippet instance. We can set this 
-    # automatically by overriding .perform_create() method on our snippet 
-    # views that lets us modify how an instance is saved.
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    
     
